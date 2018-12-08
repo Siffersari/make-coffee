dd3bfd15bfbbf8b91c807d903483ac1af8c3fdba
LeewelKarani <Leewelkarani@gmail.com>
2018-12-08 12:33:49 +0300

    Add gitignore and coffee v1

---
+++ coffee.py
@@ -0,0 +1,18 @@
+""" Instructions for coffee machine
+    1. Make and serve me, you and Gibbs a cup of coffee (add coffee, add hot water, stir)
+    2. Change how the mix is stirred
+    3. A better way to make coffee with less repitition
+    4. Make you coffee with milk and sugar (add sugar, add milk)
+    5. Make Gibbs coffee with milk, sugara and something else (add sugar, add milk, ...)
+    6. Refactor
+"""
+
+# Make my coffee
+ingredients = ['coffee', 'hot water']
+print('Started making coffee...')
+print('Getting cup')
+print('Adding {}'.format(', '.join(ingredients)))
+print('Stir the mix')
+print('Finished making coffee...')
+my_coffee = 'Tasty coffee'
+print("---Here's your {} {}, Enjoy!! ---\n".format(my_coffee, 'Evans'))
